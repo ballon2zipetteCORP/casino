d7bdeec9ee8ebd2a481068d90253973ee5663077
KazouDev <frenchkazou@gmail.com>
2025-03-24 10:48:38 +0100

    fix roulette

--- src/components/games/wheel/GameWheel.vue
+++ src/components/games/wheel/GameWheel.vue
@@ -1,95 +1,107 @@
 <template>
-    <BaseGame :landscape-required="true">
-        <div class="center">
-            <game-wheel-spin :actual-players-connected="actualPlayersConnected" :next-spin="nextSpin" @on-result="showResult" ref="spinRef" />
-            <game-wheel-table ref="tableRef" />
-        </div>
-    </BaseGame>
+  <BaseGame :landscape-required="true">
+    <div class="center">
+      <game-wheel-spin
+        :actual-players-connected="actualPlayersConnected"
+        :next-spin="nextSpin"
+        @on-result="showResult"
+        ref="spinRef"
+      />
+      <game-wheel-table ref="tableRef" />
+    </div>
+  </BaseGame>
 </template>
 
 <script lang="ts" setup>
-import { onBeforeUnmount, ref, watch } from 'vue';
+import { onBeforeUnmount, ref, watch } from "vue";
 
-import GameWheelTable from './GameWheelTable.vue';
-import GameWheelSpin from './GameWheelSpin.vue';
+import GameWheelSpin from "./GameWheelSpin.vue";
+import GameWheelTable from "./GameWheelTable.vue";
 
-import { useWebsocketStore } from '@/stores/useWebsocketStore';
-import { storeToRefs } from 'pinia';
-import { useAuthenticationStore } from '@/stores/useAuthenticationStore';
-import BaseGame from '../BaseGame.vue';
+import { useAuthenticationStore } from "@/stores/useAuthenticationStore";
+import { useWebsocketStore } from "@/stores/useWebsocketStore";
+import { storeToRefs } from "pinia";
+import BaseGame from "../BaseGame.vue";
 
 const { me } = storeToRefs(useAuthenticationStore());
 
-const spinRef = ref<typeof GameWheelSpin|null>(null);
-const tableRef = ref<typeof GameWheelTable|null>(null);
+const spinRef = ref<typeof GameWheelSpin | null>(null);
+const tableRef = ref<typeof GameWheelTable | null>(null);
 
-const nextSpin = ref<Date|null>(null);
+const nextSpin = ref<Date | null>(null);
 const actualPlayersConnected = ref<number>(0);
 
 const amountWon = ref<number>(0);
 const hasLost = ref<boolean>(false);
 
-const handleMessages = () => {  
-    useWebsocketStore().addMessageListener(message => {
-        switch(message.type) {
-            case "INFORMATION":
-                nextSpin.value = new Date(message.data?.nextSpin);
-                actualPlayersConnected.value = parseInt(message.data?.actualPlayersConnected);
-                break;
-            case "START_SPINNING":
-                spinRef?.value?.spin(message.data?.number);
-                break;
-            case "REWARD":
-                amountWon.value = message.data?.amount ?? 0;
-                break;
-            case "BET_LOST":
-                hasLost.value = true;
-                break;
-        }
-    });
-}
+const handleMessages = () => {
+  useWebsocketStore().addMessageListener((message) => {
+    switch (message.type) {
+      case "INFORMATION":
+        nextSpin.value = new Date(message.data?.nextSpin);
+        actualPlayersConnected.value = parseInt(
+          message.data?.actualPlayersConnected
+        );
+        break;
+      case "START_SPINNING":
+        spinRef?.value?.spin(message.data?.number);
+        break;
+      case "REWARD":
+        amountWon.value = message.data?.amount ?? 0;
+        break;
+      case "BET_LOST":
+        hasLost.value = true;
+        break;
+    }
+  });
+};
 
 const showResult = () => {
-    if(amountWon.value > 0) {
-        me.value!.zipetteCoins += amountWon.value;
-        window.toast({
-            level: "INFO",
-            title: "Vous avez gagné " + amountWon.value + " ZPC !"
-        });
-    } else if(hasLost.value) {
-        window.toast({
-            level: "INFO",
-            title: "Tu as perdu tous vos zipette coins...",
-            subtitle: "Grosse merde"
-        });
-    }
-    
-    reset();
-}
+  console.log("show result;");
+  if (amountWon.value > 0) {
+    me.value!.zipetteCoins += amountWon.value;
+    window.toast({
+      level: "INFO",
+      title: "Vous avez gagné " + amountWon.value + " ZPC !",
+    });
+  } else if (hasLost.value) {
+    window.toast({
+      level: "INFO",
+      title: "Tu as perdu tous vos zipette coins...",
+      subtitle: "Grosse merde",
+    });
+  }
+
+  reset();
+};
 
 const reset = () => {
-    hasLost.value = false;
-    amountWon.value = 0;
-    tableRef?.value?.resetUserSelection();
-}
+  hasLost.value = false;
+  amountWon.value = 0;
+  tableRef?.value?.resetUserSelection();
+};
 
-watch(me, value => {
-    if(value) {
-        useWebsocketStore().connect("EUROPEAN_ROULETTE");
-        handleMessages();
+watch(
+  me,
+  (value) => {
+    if (value) {
+      useWebsocketStore().connect("EUROPEAN_ROULETTE");
+      handleMessages();
     }
-}, {immediate: true});
+  },
+  { immediate: true }
+);
 
 onBeforeUnmount(() => {
-    useWebsocketStore().close();
-})
+  useWebsocketStore().close();
+});
 </script>
 
 <style scoped>
 div.center {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2em;
 }
 </style>
