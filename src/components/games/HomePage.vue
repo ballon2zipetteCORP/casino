92c71cb87f290bbe0b8bf5283e4d170f4ccc73d5
matteogaillard <matteo.gaillard@protonmail.com>
2025-03-25 15:27:39 +0100

    better home page responsive

--- src/components/games/HomePage.vue
+++ src/components/games/HomePage.vue
@@ -217,11 +217,57 @@ a.blackjack {
     a.avioncrash {
         padding-bottom: 5em;
     }
+    a.cash {
+        padding-bottom: 5em !important;
+    }
 }
 @media screen and (max-width: 646px) {
     section {
         display: flex;
         flex-direction: column;
+    }
+}
+@media screen and (max-width: 568px) {
+    a.cash {
+        img {
+            width: 13em;
+        }
+    }
+    a.tresor {
+        h2 {
+            padding-bottom: 3em;
+        }
+        img {
+            width: 10em;
+        }
+    }
+    a.machine {
+        img {
+            width: 8em;
+        }
+        h2 {
+            font-size: 1.7em;
+            padding-bottom: 3em;
+        }
+    }
+    a.avioncrash, a.blackjack {
+        img {
+            width: 12em;
+        }
+    }
+    a.avioncrash {
+        h2 {
+            padding-bottom: 3em;
+        }
+    }
+    a.blackjack {
+        img {
+            right: 0;
+            top: 6em;
+        }
+        h2 {
+            padding-bottom: 5em;
+        }
     }
 }
 </style>
